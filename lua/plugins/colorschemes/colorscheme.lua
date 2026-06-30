b314848858e0fb35d5249f79df1c95f0b262b75f
Kaboupi <gnovozhiloff@gmail.com>
2026-06-30 14:30:47 +0300

    Add: more colorschemes

--- lua/plugins/colorschemes/colorscheme.lua
+++ lua/plugins/colorschemes/colorscheme.lua
@@ -2,16 +2,49 @@ return {
   -- Transaprent
   { "xiyaowong/transparent.nvim" },
 
-  { "ellisonleao/gruvbox.nvim" },
-  { "rebelot/kanagawa.nvim" },
-  { "EdenEast/nightfox.nvim" },
-  { "nyoom-engineering/oxocarbon.nvim" },
+  -- Colorschemes
+  {
+    "sainnhe/everforest",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_enable_italic = true
+      vim.g.gruvbox_material_background = "medium" -- hard / medium / soft
+    end,
+  },
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_enable_italic = true
+      vim.g.gruvbox_material_background = "hard" -- hard / medium / soft
+    end,
+  },
+  {
+    "sainnhe/sonokai",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.sonokai_style = "andromeda" -- default, atlantis, andromeda, shusia, maia
+      vim.g.sonokai_better_performance = 1 -- 1 / 0
+    end,
+  },
 
-  -- gruvbox / terafox / nightfox
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+
+  { "ellisonleao/gruvbox.nvim" },
+
+  { "EdenEast/nightfox.nvim" },
+
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nightfox",
+      colorscheme = "gruvbox-material",
     },
   },
 }
